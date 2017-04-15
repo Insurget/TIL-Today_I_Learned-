655b63026008840c3e0d40f77391c3aff2030f2d
Ki-Hin <in010210@gmail.com>
2017-04-15 14:21:39 +0900

    2920번 음계

--- baekjoon/2920음계.c
+++ baekjoon/2920음계.c
@@ -2,22 +2,16 @@
 
 int main(void)
 {
-	int a = 0, i;
-	int string[8];
-	for (i = 0; i < 8; i++)
-	scanf("%d", &string[i]);
-	for (i = 0; i < 7; i++)
-	{
-		if (string[i+1]-string[i]==1)
-			a++;
-		else 
-			a--;
+	int n[8],ans=0;
+	scanf("%d %d %d %d %d %d %d %d", &n[0], &n[1], &n[2], &n[3], &n[4], &n[5], &n[6], &n[7]);
+	for (int i = 0; i < 7; i++) {
+		if (n[i] == n[i + 1] + 1)
+			ans--;
+		else if (n[i] == n[i + 1] - 1)
+			ans++;
+		else break;
 	}
-	if (a == 7)
-	printf("ascending");
-	else if (a == -7)a
-	printf("descending");
-	else
-	printf("mixed");
-	return 0;
+	if (ans == 7)printf("ascending");
+	else if (ans == -7)printf("descending");
+	else printf("mixed");
 }
