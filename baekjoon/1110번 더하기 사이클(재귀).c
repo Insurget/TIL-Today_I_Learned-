983d33c570604c3784463c12e840f55e6838c13f
Ki-Hin <in010210@gmail.com>
2017-04-26 14:54:53 +0900

    add 1110 solving

---
+++ baekjoon/1110번 더하기 사이클(재귀).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int f(int S_value, int sip, int ill, int n)
+{
+	printf("%d + %d = %d, %d\n n = %d\n",sip,ill,sip+ill, ill*10+((sip+ill)%10), n);
+	if (sip * 10 + ill == S_value&&n!=0)return printf("%d\n", n);
+	sip = (sip + ill) % 10;
+	return f(S_value, ill, sip, ++n);
+}
+int main(void)
+{
+	int start;
+	while (1)
+	{
+		scanf("%d", &start);
+		if (start < 10)f(start * 10, start, 0, 0);
+		f(start, start / 10, start % 10, 0);
+	}
+}
