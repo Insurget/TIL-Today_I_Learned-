03e530da8b8e0db9f739521fb2e409f8d25a9e52
Ki-Hin <in010210@gmail.com>
2017-04-27 22:36:31 +0900

    Solve 14467(complete)

--- baekjoon/14467번 소가 길을 건너간 이유.c
+++ baekjoon/14467번 소가 길을 건너간 이유.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-	int n, cow[11], see_cow, see_pos, cnt=0;
+	int n, cow[11] = { 2,2,2,2,2,2,2,2,2,2,2 }, cow_num, cow_pos, cnt = 0;
 	scanf("%d", &n);
 	while (n--)
 	{
-		scanf("%d %d", &see_cow, &see_pos);
+		scanf("%d %d", &cow_num, &cow_pos);
 
-		if ( cow[see_cow] != see_pos && (cow[see_cow] == 0 || cow[see_cow] == 1 )) cnt++;
-		cow[see_cow] = see_pos;
+		if (cow[cow_num] != cow_pos && (cow[cow_num] == 0 || cow[cow_num] == 1)) cnt++;
+		cow[cow_num] = cow_pos;
 	}
 	printf("%d", cnt);
 }
