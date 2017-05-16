2d8cc894040128991a00728adaa101cd0ecc7901
Ki-Hin <in010210@gmail.com>
2017-05-16 20:32:14 +0900

    Update a

--- algorithm/BOJ/2017연세대프로그래밍 경진대회 - A.c
+++ algorithm/BOJ/2017연세대프로그래밍 경진대회 - A.c
@@ -2,18 +2,47 @@
 
 int main(void)
 {
-	int n, cnt = 0;
+	int n;
 	scanf("%d", &n);
 
-	for (int i = 2; i < n-3; i += 2)
+	printf("%d ", Logic1(n));
+	printf("%d", Logic2(n));
+}
+
+int Logic1(int n)	//O(N^3)
+{
+	int cnt = 0;
+	for (int i = 2; i < n - 3; i += 2)
 	{
 		for (int j = 1; j < n - i; j++)
 		{
-			for (int k = j+2; i + j + k <= n; k++)
+			for (int k = j + 2; i + j + k <= n; k++)
 			{
 				if (i + j + k == n)cnt++;
 			}
 		}
 	}
-	printf("%d", cnt);
+	return cnt;
+}
+
+int Logic2(int n)	//O(N^2)
+{
+	int cnt = 0;
+	for (int i = 1; i < n - 3; i++)
+	{
+		for (int j = i + 2; i + j < n - 2; j++)
+		{
+			if ((n - j - i) % 2 == 0)cnt++;
+		}
+	}
+	return cnt;
+}
+
+int Logic3(int n)	//O(N)
+{
+	int cnt = 0;
+	for (int i = 2; i < n-3; i += 2)
+	{
+		 nj, 
+	}
 }
