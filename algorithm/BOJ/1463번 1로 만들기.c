eddc9a28b87bee359390842aab9af38aeaf9f18f
Ki-Hin <in010210@gmail.com>
2017-06-21 19:48:42 +0900

    [BOJ]Complete solving 1463 - 1로 만들기

---
+++ algorithm/BOJ/1463번 1로 만들기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int min = 100000000;
+
+int f(int x, int count) {
+	if (count >= min)return;
+	if (x < 1)return;
+	if (x == 1) {
+		if (min > count)min = count;
+		return;
+	}
+	if (x % 3 == 0)f(x / 3, count+1);
+	if (x % 2 == 0)f(x / 2, count+1);
+	f(--x, count+1);
+	return min;
+}
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	if (n == 1)printf("1");
+	else printf("%d", f(n, 0));
+}
