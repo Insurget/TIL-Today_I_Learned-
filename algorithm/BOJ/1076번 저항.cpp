b17b7fe174ec844cf58411b98d1fbda8fd3cbeaf
Ki-Hin <in010210@gmail.com>
2017-07-31 22:12:55 +0900

    [PS] Solving complete 1076(BOJ)

---
+++ algorithm/BOJ/1076번 저항.cpp
@@ -0,0 +1,17 @@
+
+int main()
+{
+	map<string, int> n = {
+	   {"black", 0}, {"brown",1},{"red",2},
+	   {"orange",3}, {"yellow",4},{"green",5},
+	   {"blue",6},{"violet",7},{"grey",8},
+	   {"white",9} };
+
+	string a, b, c;
+	cin >> a >> b >> c;
+	long long tot = (long long)(n[a] * 10 + n[b]);
+	for (int i = 0; i < n[c]; i++) {
+		tot *= 10LL;
+	}
+	cout << tot<<'\n';
+}
